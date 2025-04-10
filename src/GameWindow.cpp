3488b30d1e4e704df2e6bce20dab5ddc5b3a36b1
Firat Ozkan <ext-firat.ozkan@vaisala.com>
2025-04-10 17:56:20 +0300

    Major progress

--- src/GameWindow.cpp
+++ src/GameWindow.cpp
@@ -1,121 +1,62 @@
-/**
- * @file GameWindow.cpp
- * @brief Implements the GameWindow class for managing the game window and loop.
- */
+#include "GameWindow.hpp"
+#include "Tile.hpp"
+#include "Player.hpp"
+#include "Camera.hpp"
 
-#include <thread>
-#include <stdexcept>
-#include <SDL2/SDL.h>
-#include "Core/GameWindow.hpp"
-#include "States/Levels/Level_1.hpp"
-
-using namespace Core;
-
-/**
- * @brief Constructs a GameWindow and initializes SDL components.
- *
- * The constructor initializes SDL components, including the window and renderer.
- * It also sets up the initial game state.
- */
 GameWindow::GameWindow()
+    : m_window(sf::VideoMode(static_cast<unsigned int>(GameWindow::GAME_WIDTH),
+                             static_cast<unsigned int>(GameWindow::GAME_HEIGHT)),
+               "Game Demo")
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-        throw std::runtime_error("Couldn't initialize SDL!");
-
-    m_window = SDL_CreateWindow("Game",
-                                SDL_WINDOWPOS_UNDEFINED,
-                                SDL_WINDOWPOS_UNDEFINED,
-                                800,
-                                600,
-                                SDL_WINDOW_SHOWN);
-
-    if (!m_window)
-    {
-        SDL_Quit();
-        throw std::runtime_error("Couldn't create window!");
-    }
-
-    if (!SDL_CreateRenderer(m_window, -1, SDL_RENDERER_ACCELERATED))
-    {
-        SDL_DestroyWindow(m_window);
-        SDL_Quit();
-        throw std::runtime_error("Couldn't create renderer!");
-    }
+    m_window.setFramerateLimit(60);
+    m_window.setVerticalSyncEnabled(true);
 }
 
-/**
- * @brief Destructs the GameWindow and cleans up SDL resources.
- *
- * The destructor cleans up SDL resources, including the window and renderer.
- */
-GameWindow::~GameWindow()
-{
-    SDL_DestroyRenderer(SDL_GetRenderer(m_window));
-    SDL_DestroyWindow(m_window);
-    SDL_Quit();
-}
-
-/**
- * @brief Gets a reference to the GameWindow instance.
- *
- * The get method provides access to the singleton instance of the GameWindow class.
- *
- * @return A reference to the GameWindow instance.
- */
 GameWindow& GameWindow::get()
 {
     static GameWindow instance;
     return instance;
 }
 
-/**
- * @brief Runs the game loop.
- *
- * The run method starts the game loop, handling events, updating the current game state,
- * and rendering frames. It also includes frame rate limiting.
- */
 void GameWindow::run()
 {
-    m_currentState = std::make_unique<States::Levels::Level_1>();
+    sf::Clock clock;
 
-    while (m_gameRunning)
+    Tile tile;
+    Player player;
+    auto& camera = Camera::get();
+
+    while (m_window.isOpen())
     {
-        const auto startTime = std::chrono::steady_clock::now();
+        sf::Event event;
+        while (m_window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                m_window.close();
+            else
+                player.handleInputs(event);
+        }
 
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-            m_currentState->handleInput(event);
+        if (tile.isColliding(player))
+            tile.applyCollision(player);
 
-        m_currentState->updateState(std::chrono::milliseconds(10));
-
-        m_currentState->renderState();
-
-        using namespace std::chrono_literals;
-
-        const auto elapsed = std::chrono::steady_clock::now() - startTime;
-        if (elapsed < 16.667ms)
-            std::this_thread::sleep_for(16.667ms - elapsed);
+        const float deltaTime = clock.restart().asSeconds();
+        player.update(deltaTime);
+        camera.update(player);
+        
+        m_window.clear();
+        tile.draw();
+        player.draw();
+        m_window.display();
     }
 }
 
-/**
- * @brief Signals the game to quit.
- *
- * The quit method sets the gameRunning flag to false, which will terminate the game loop.
- */
-void GameWindow::quit()
+sf::RenderWindow& GameWindow::getWindow()
 {
-    m_gameRunning = false;
+    return m_window;
 }
 
-/**
- * @brief Gets the SDL renderer associated with the game window.
- *
- * The getRenderer method returns a pointer to the SDL renderer used for rendering graphics.
- *
- * @return A pointer to the SDL renderer.
- */
-SDL_Renderer* GameWindow::getRenderer() const
+const sf::RenderWindow& GameWindow::getWindow() const
 {
-    return SDL_GetRenderer(m_window);
+    return m_window;
 }
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,24 +1,7 @@
-/**
- * @file main.cpp
- * @brief Entry point for the game application.
- */
+#include "GameWindow.hpp"
 
-#include "Core/GameWindow.hpp"
-
-/**
- * @brief The entry point of the game application.
- *
- * The main function initializes the game window and starts the game loop by calling
- * the run method of the GameWindow singleton instance.
- *
- * @return The exit code of the application.
- */
 int main()
 {
-    // Initialize the game window and start the game loop.
-    Core::GameWindow::get().run();
-
-    // The game loop handles the application's execution, so no further code execution occurs here.
-
-    return 0; // Return an exit code (0 for success).
+    GameWindow::get().run();
+    return 0;
 }
